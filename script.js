9b7dc34949499185c09957b13aa641562198f389
dennisrev <65338270+dennisrev@users.noreply.github.com>
2026-06-12 15:58:12 +0200

    Started with JS part

---
+++ script.js
@@ -0,0 +1,13 @@
+const amounts = document.getElementsByClassName("amount");
+
+function message() {
+    console.log('Aantal producten is gewijzigd');
+}
+
+amounts.forEach(myFunction)
+
+function myFunction {
+    
+}
+
+amounts.addEventListener("change", message);
